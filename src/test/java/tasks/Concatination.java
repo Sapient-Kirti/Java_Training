e26ea304fa852418665a8be8fad61ff98d6e3430
Kirti Chaudhary <kirchaud1@publicisgroupe.net>
2022-11-30 09:20:57 +0530

    Added files

---
+++ src/test/java/tasks/Concatination.java
@@ -0,0 +1,25 @@
+package tasks;
+
+import java.util.Scanner;
+
+import org.testng.annotations.Test;
+
+
+public class Concatination {
+	 @Test
+	public static void main(String[] args) {
+		
+		//System.out.println("Enter the first name:");
+		
+		//String fname=scan.nextLine();
+		
+		//System.out.println("Enter the last name:");
+		
+		//String lname=scan.nextLine();
+		
+		System.out.println("Hello" + " "+ "Kirti" + " " + "Chaudhary");		
+		
+
+	}
+
+}
